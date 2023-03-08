3c1211234b9303c5eff5768c83fe6ee96311fe2c
gandlasaisubramanyam <124264006+gandlasaisubramanyam@users.noreply.github.com>
2023-03-08 13:40:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+setTimeout(function(){
+    document.getElementById("demo").innerHTML = "10";
+console.log("10");
+setTimeout(function(){
+    document.getElementById("demo").innerHTML = "9";
+    console.log("9"); 
+    setTimeout(function(){
+        document.getElementById("demo").innerHTML = "8";
+        console.log("8");  
+        setTimeout(function(){
+            document.getElementById("demo").innerHTML = "7";
+            console.log("7"); 
+            setTimeout(function(){
+                document.getElementById("demo").innerHTML = "6";
+                console.log("6"); 
+                setTimeout(function(){
+                    document.getElementById("demo").innerHTML = "5";
+                    console.log("5");   
+                    setTimeout(function(){
+                        document.getElementById("demo").innerHTML = "4";
+                        console.log("4");   
+                        setTimeout(function(){
+                            document.getElementById("demo").innerHTML = "3";
+                            console.log("3");  
+                            setTimeout(function(){
+                                document.getElementById("demo").innerHTML = "2";
+                                console.log("2");   
+                                setTimeout(function(){
+                                    document.getElementById("demo").innerHTML = "1";
+                                    console.log("1");    
+                                setTimeout(function(){
+                                    document.getElementById("demo").innerHTML = "Happy Independence Day";
+                                    console.log("Happy Independence Day");   
+                                },1000)  
+                            },1000)
+                            },1000)  
+                        },1000 )
+                    },1000) 
+                },1000)   
+            },1000)   
+        },1000) 
+    },1000)  
+},1000)
+},1000)
